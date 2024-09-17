edd7cd3425b9d70c38b7ddd43caed71778b609be
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-09-17 13:50:30 +0000

    Ezhil

--- PP1_3.py
+++ PP1_3.py
@@ -1,14 +1,27 @@
-
+''' 
+    Lesson 3 - Using Strings
+    Author: Ezhil S.
+    Date Created: September 17th, 2024 
+    Date Last Modified: September 17th, 2024 
+'''
 
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *")
+  print("\n* * * * *\n*       *\n*       *\n*       *\n* * * * *")
+  print("\n* * * * * *\n *       * \n  *     *\n   *   * \n    * *\n     *")
+  print()
 def q2():
   #Write Assignment code here
-  
+  word1 = "Hello "
+  word2 = "World"
+  print(word1+word2)
 def q3():
   #Write Assignment code here
+  print("Man\nBoard\nMan Overboard")
+
+
   
 
 
